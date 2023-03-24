2a9375f4c64879d1f7b98d5d84331d0c8a9f5be9
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2023-03-24 17:43:46 +0100

    add examples, run ci

---
+++ example/src-gen/deploy_pick_n_place_to_ur5e_cell_with_rostooling/realsense_driver/realsense_driver_rwu_laptop.sh
@@ -0,0 +1,1 @@
+apt install ros-humble-realsense2-camera -qq -y
---
+++ example/src-gen/deploy_pick_n_place_to_ur5e_cell_with_rostooling/ur5e_driver/ur5e_driver_rwu_laptop.sh
@@ -0,0 +1,6 @@
+apt install ros-humble-force-torque-sensor-broadcaster -qq -y
+apt install ros-humble-joint-trajectory-controller -qq -y
+apt install ros-humble-position-controllers -qq -y
+apt install ros-humble-robot-state-publisher -qq -y
+apt install ros-humble-ur-controllers -qq -y
+apt install ros-humble-ur-robot-driver -qq -y
